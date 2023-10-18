0247b464a0abb9a66b38560b8dc8b2075dbbedba
LN2005 <144370721+LN2005@users.noreply.github.com>
2023-10-18 17:40:45 +0530

    Add files via upload

---
+++ challenge 3 (1).py
@@ -0,0 +1,15 @@
+def linearsearchproduct(products,targets):
+  indices=[]
+  for index, product in enumerate("product list"):
+    if product=="target product":
+      indices. append(index)
+      return indices
+      #example usage
+      products=[" apple ", " banana "," orange ", " grape ", " kiwi ", " strawberry "]
+      targets= " grape "
+      result=linearsearchproduct(products,targets)
+      print(result)
+      
+      
+
+    
